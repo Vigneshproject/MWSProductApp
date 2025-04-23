babc0a4540a64821dd3b4170e71031c04eff57bd
Vignesh Saravanan <vignesh.saravanan@Trionfo.com>
2025-04-23 23:17:16 +0530

    Add Register/Login Model

---
+++ Model/MWSAdminRegister.cs
@@ -0,0 +1,19 @@
+﻿namespace MWSProductApp.Model
+{
+    public class MWSAdminRegister
+    {
+        public string? Role { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Salutation { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? Suffix { get; set; }
+        public string? Gender { get; set; }
+        public string? MaritalStatus { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? UserId { get; set; }
+    }
+}
